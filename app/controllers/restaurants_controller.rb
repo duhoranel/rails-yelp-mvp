54423c79fee102ca9171a9df4a68bd256baffcae
Laure Duhorane <laure.duhorane@lewagon.org>
2020-08-11 21:42:17 -0400

    review controller fixed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,8 +9,13 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+    # @restaurant.save
+    # redirect_to restaurant_path(@restaurant)
   end
 
   def index
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,7 +15,9 @@ class ReviewsController < ApplicationController
     end
   end
 
+  private
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
